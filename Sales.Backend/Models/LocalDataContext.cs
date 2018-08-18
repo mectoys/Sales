df815a3251fb6ef1fc626267b85bc25bd31f3908
JOSE ALFREDO PONCIANO CARRANZA <mectoys2013@gmail.com>
2018-08-18 00:08:02 -0500

    Sales 18/08/2018

---
+++ Sales.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,12 @@
+﻿ 
+
+namespace Sales.Backend.Models
+{
+    using Domain.Models;
+
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
+        //facilitamos lamigracion de la BD por asistente en el controlador
+    }
+}
